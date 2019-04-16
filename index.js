92e02a34e5bebfe54a6336aefff4d12f9ee4b769
Danilo Pedrosa <danilomopedrosa@gmail.com>
2019-04-15 22:45:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,89 @@
 import rp from 'request-promise'
 
-const captcha = key => {
-  const postCaptcha = image => {
-    const options = {
-      method: 'POST',
-      url: 'http://2captcha.com/in.php',
-      qs: {
-        key: key,
-        json: '1',
-        method: 'base64'
-      },
-      headers: {
-        'cache-control': 'no-cache',
-        'content-type': 'multipart/form-data'
-      },
-      formData: {
-        body: image
-      }
-    }
+const timeoutDelay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-    return rp(options)
-  }
+const captcha = (key) => {
+	const MAX_TRIES_POST = 5;
+	const MAX_TRIES_GET = 10;
 
-  const getCaptcha = async id => {
-    const options = {
-      method: 'GET',
-      url: 'http://2captcha.com/res.php',
-      qs: {
-        key: key,
-        action: 'get',
-        id,
-        json: '1'
-      }
-    }
+	let triesPost = 0;
+	let triesGet = 0;
+	let captchaId;
 
-    return rp(options)
-  }
+	const postCaptcha = async (image) => {
+		const options = {
+			method: 'POST',
+			url: 'http://2captcha.com/in.php',
+			qs: {
+				key: key,
+				json: '1',
+				method: 'base64'
+			},
+			headers: {
+				'cache-control': 'no-cache',
+				'content-type': 'multipart/form-data'
+			},
+			formData: {
+				body: image
+			},	
+    		json: true
+		}
 
-  return {
-    solve: async image => {
-      const postRequest = await postCaptcha(image)
+        try {
+			const result = await rp(options);
+			captchaId = result.request;
+        } catch(error) {
+            if (triesPost < MAX_TRIES_POST) {
+                triesPost++;
+                return postCaptcha(image);
+			}
 
-      const JSONPost = JSON.parse(postRequest)
+			return error;
+        }
+	}
 
-      const id = JSONPost.request
+	const getCaptcha = (id) => {
+		const options = {
+			method: 'GET',
+			url: 'http://2captcha.com/res.php',
+			qs: {
+				key: key,
+				action: 'get',
+				id,
+				json: '1'
+			},	
+    		json: true
+		}
+		
+		try {
+			const result = await rp(options);
+			
+			if (result.status !== 1 && triesGet < MAX_TRIES_GET) {
+				triesGet++;
+				timeoutDelay(3000);
+                return getCaptcha(id);
+			}
+			return result.request;
+        } catch(error) {
+            if (triesGet < MAX_TRIES_GET) {
+				triesGet++;
+				timeoutDelay(3000);
+                return getCaptcha(id);
+			}
 
-      await new Promise(resolve => setTimeout(resolve, 5000))
-
-      let response = false
-
-      while (!response) {
-        const getRequest = await getCaptcha(id)
-        const JSONGet = JSON.parse(getRequest)
-
-        if (JSONGet.status === 1) {
-          response = true
-          return JSONGet.request
+			return error;
         }
 
-        await new Promise(resolve => setTimeout(resolve, 1000))
-      }
-    }
-  }
+	}
+
+	const solveCaptcha = async (image) => {
+		await postCaptcha(image);
+		return await getCaptcha(captchaId);
+	}
 }
 
-export default captcha
+return {
+	solve: solveCaptcha
+}
+
+export default captcha;
