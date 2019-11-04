dfa0788f5a9378e806be617c6fd86f4c96c800fd
Pedro Filho <pedro@filho.me>
2019-11-04 15:34:51 -0300

    migrating to typescript

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import rp from "request-promise-native";
+
+const postUrl = "http://2captcha.com/in.php";
+
+const getUrl = "http://2captcha.com/res.php";
+
+const timer = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const captchaSolver = (key: string) => {
+  const postCaptcha = async image => {
+    const options = {
+      method: "POST",
+      url: postUrl,
+      qs: {
+        key: key,
+        json: "1",
+        method: "base64"
+      },
+      headers: {
+        "cache-control": "no-cache",
+        "content-type": "multipart/form-data"
+      },
+      formData: {
+        body: image
+      }
+    };
+
+    try {
+      const postRequest = await rp(options);
+
+      const JSONPost = JSON.parse(postRequest);
+
+      return JSONPost.request;
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  const getCaptcha = async (id: string) => {
+    const options = {
+      method: "GET",
+      url: getUrl,
+      qs: {
+        key: key,
+        action: "get",
+        id,
+        json: "1"
+      }
+    };
+
+    try {
+      const getRequest = await rp(options);
+
+      const JSONGet = JSON.parse(getRequest);
+
+      if (JSONGet.status === 1) {
+        return JSONGet.request;
+      }
+
+      await timer(1000);
+
+      return getCaptcha(id);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  const solveCaptcha = async image => {
+    const id = await postCaptcha(image);
+
+    await timer(5000);
+
+    return getCaptcha(id);
+  };
+
+  const getBalance = async () => {
+    const options = {
+      method: "GET",
+      url: getUrl,
+      qs: {
+        key: key,
+        action: "getbalance",
+        json: "1"
+      }
+    };
+
+    try {
+      const getBalance = await rp(options);
+
+      const JSONBalance = JSON.parse(getBalance);
+
+      return JSONBalance.request;
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  return {
+    solve: solveCaptcha,
+    balance: getBalance
+  };
+};
+
+export default captchaSolver;
